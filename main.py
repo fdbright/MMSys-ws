910d9fb9a9b2862b8652269c3f6eb11116a1171f
aiden <aiden.shi.hai@gmail.com>
2023-04-28 15:58:06 +0800

    修改路由

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ sys.path.append("/home/ec2-user/MMSys-ws")
 
 from loguru import logger as log
 
-import tornado.web
-import tornado.ioloop
 import tornado.httpserver
+from tornado.web import Application
+from tornado.ioloop import IOLoop
 from tornado.options import define, options
+from tornado.routing import Rule, RuleRouter, PathMatches
 
 from App import Login, ReportRest
 from Route import V1MainRoute
@@ -23,19 +24,28 @@ options.parse_command_line()
 
 log.add(Configure.LOG_MAIN, retention="2 days", encoding="utf-8")
 
-urls = [
-    ("/app/wss/v1", V1MainRoute),
-    ("/app/htp/v1/user/login", Login),
-    ("/app/htp/v1/report", ReportRest),
-]
-app = tornado.web.Application(
-    urls,
+htp_app = Application(
+    [
+        ("/app/htp/v1/user/login", Login),
+        ("/app/htp/v1/report", ReportRest),
+    ],
+    debug=True
+)
+wss_app = Application(
+    [
+        ("/app/wss/v1", V1MainRoute),
+    ],
     debug=True,
     # websocket_ping_interval=5,  # 间隔5秒发送一次ping帧，第一次发送为触发的5s后
     # websocket_ping_timeout=60,  # 每次 ping 操作重置时间超时时间，若超时则断开连接，默认3次 ping 或 30s 断开
 )
 
-httpserver = tornado.httpserver.HTTPServer(app)
+router = RuleRouter([
+    Rule(PathMatches("/app/htp/.*"), htp_app),
+    Rule(PathMatches("/app/wss/.*"), wss_app),
+])
+
+httpserver = tornado.httpserver.HTTPServer(router)
 httpserver.listen(options.port)
 
 if __name__ == '__main__':
